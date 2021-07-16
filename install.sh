13861c56041748202fddc8446c0c0a409f1bb869
Mao Bunthon <bunthon@Maos-MacBook-Pro.local>
2021-07-16 19:34:36 +0700

    [INIT] First project

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Setting up your Mac Home..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+# brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
